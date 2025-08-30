90740231b6879ab05c48394b42818b5cdbf38091
EightBitBanger <66331916+EightBitBanger@users.noreply.github.com>
2025-08-30 10:48:03 -0600

    Delete src directory

--- src/transformer/Activation.h
+++
@@ -1,8 +0,0 @@
-#ifndef ACTIVATION_FUNCTIONS_H
-#define ACTIVATION_FUNCTIONS_H
-
-// GELU approx and backward
-void GELU_InPlace(Tensor2D& x);
-void GELU_Backward(const Tensor2D& x, const Tensor2D& dy, Tensor2D& dx);
-
-#endif
